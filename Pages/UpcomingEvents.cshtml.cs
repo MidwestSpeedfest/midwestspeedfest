1b155aaf12f63dd0f144a4d57b04da7aa423a109
Dan Hunt <Daniel.Hunt@sumtotalsystems.com>
2020-08-31 14:04:27 -0500

    Moved parentstring for Twitch Embeds to codebehind

--- Pages/UpcomingEvents.cshtml.cs
+++ Pages/UpcomingEvents.cshtml.cs
@@ -2,15 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MidwestSpeedfest.Utils;
 
 namespace MidwestSpeedfest.Pages
 {
     public class UpcomingEventsModel : PageModel
     {
+        public List<HtmlString> Cards = new List<HtmlString>();
+
         public void OnGet()
         {
+            PopulateUpcomingEventCards();
+        }
+
+        private void PopulateUpcomingEventCards()
+        {
+            Cards.Add(
+                new UpcomingEventCard
+                {
+                    Title = "MidFall SpeedBall",
+                    ImageUrl = null,
+                    AltText = null,
+                    Description = "Midfall Speedball will be a charity speedrun marathon running from November 20-22, 2020. <br/> <br/>More details will be announced soon!",
+                    RunnerApplicationUrl = null
+                }.GetCardHtml()
+            );
         }
     }
 }
