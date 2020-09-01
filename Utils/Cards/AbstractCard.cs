86153894a3c2d499c4e10b5c014d0845fee669bb
Dan Hunt <Daniel.Hunt@sumtotalsystems.com>
2020-09-01 07:44:26 -0500

    Adding StaffBio card & card inheritance

---
+++ Utils/Cards/AbstractCard.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace MidwestSpeedfest.Utils.Cards
+{
+    public abstract class AbstractCard : ICard
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string AltText { get; set; }
+        public string Description { get; set; }
+
+        public abstract HtmlString GetCardHtml();
+    }
+}
---
+++ Utils/Cards/StaffBioCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Html;
+
+namespace MidwestSpeedfest.Utils.Cards
+{
+    public class StaffBioCard : AbstractCard
+    {
+        public override HtmlString GetCardHtml()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(@"<div class=""card upcoming-events-card bg-dark"">");
+            if (!string.IsNullOrEmpty(ImageUrl))
+            {
+                sb.AppendLine(@"    <div class=""col-12"">");
+                sb.AppendLine($@"       <img class=""card-img-top"" src={ImageUrl} alt={AltText}");
+                sb.AppendLine(@"    </div>");
+            }
+            sb.AppendLine(@"    <div class=""card-body"">");
+            sb.AppendLine($@"       <h4 class=""card-title"">{Title}</h4>");
+            sb.AppendLine($@"       <p class=""card-text"">{Description}</p>");
+            sb.AppendLine(@"    </div>");
+            sb.AppendLine(@"</div>");
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
