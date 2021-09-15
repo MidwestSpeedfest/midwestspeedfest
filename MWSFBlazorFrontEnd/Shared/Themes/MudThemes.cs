dcc3814b4f3ee1a8e31704683942e8cc641d0f31
marquisdan <marquisdan@hotmail.com>
2021-09-15 18:26:56 -0500

    Restructure shared components folder

---
+++ MWSFBlazorFrontEnd/Shared/Themes/MudThemes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MudBlazor;
+
+namespace MWSFBlazorFrontEnd.Shared.Themes
+{
+    internal static class MudThemes
+    {
+
+        internal static MudTheme DefaultMudTheme { get; }
+
+        static MudThemes()
+        {
+            DefaultMudTheme = new MudTheme();
+        }
+
+    }
+}
