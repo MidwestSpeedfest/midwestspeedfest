077429e598213771b162a6e46d9ca2e7be83b1bd
marquisdan <marquisdan@hotmail.com>
2021-09-16 14:11:33 -0500

    Start moving main layout to MudBlazor

--- MWSFBlazorFrontEnd/Shared/Themes/MudThemes.cs
+++ MWSFBlazorFrontEnd/Shared/Themes/MudThemes.cs
@@ -6,14 +6,45 @@ using MudBlazor;
 
 namespace MWSFBlazorFrontEnd.Shared.Themes
 {
-    internal static class MudThemes
+    public static class MudThemes
     {
 
         internal static MudTheme DefaultMudTheme { get; }
+        public static MudTheme MidwestSpeedfest { get; }
 
         static MudThemes()
         {
             DefaultMudTheme = new MudTheme();
+            MidwestSpeedfest = new MudTheme
+            {
+                LayoutProperties = new LayoutProperties(),
+                Palette = new Palette
+                {
+                    Black = "#27272f",
+                    Background = "#32333d",
+                    BackgroundGrey = "#27272f",
+                    Surface = "#373740",
+                    DrawerBackground = "#55C6FF",
+                    DrawerText = "rgba(255,255,255, 0.90)",
+                    DrawerIcon = "rgba(255,255,255, 0.90)",
+                    AppbarBackground = "#526FFF",
+                    AppbarText = "rgba(255,255,255, 0.90)",
+                    TextPrimary = "rgba(255,255,255, 0.90)",
+                    TextSecondary = "rgba(255,255,255, 0.50)",
+                    ActionDefault = "#adadb1",
+                    ActionDisabled = "rgba(255,255,255, 0.26)",
+                    ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+                    Divider = "rgba(255,255,255, 0.12)",
+                    DividerLight = "rgba(255,255,255, 0.06)",
+                    TableLines = "rgba(255,255,255, 0.12)",
+                    LinesDefault = "rgba(255,255,255, 0.12)",
+                    LinesInputs = "rgba(255,255,255, 0.3)",
+                    TextDisabled = "rgba(255,255,255, 0.2)"
+                },
+                Shadows = new Shadow(),
+                Typography = new Typography(),
+                ZIndex = new ZIndex()
+            };
         }
 
     }
