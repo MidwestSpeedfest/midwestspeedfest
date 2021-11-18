90d51147d5180e204c13245a23dd695721dcb2ee
marquisdan <marquisdan@hotmail.com>
2021-11-17 21:43:45 -0600

    Basically I drank whiskey before this commit

--- MWSFBlazorFrontEnd/Models/Users/UserDisplayModel.cs
+++ MWSFBlazorFrontEnd/Models/Users/UserDisplayModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using MudBlazor;
 
@@ -9,6 +11,37 @@ namespace MWSFBlazorFrontEnd.Models.Users
         internal IdentityUser User { get; set; }
         internal List<string> SelectedRoles { get; set; }
         internal MudChip[] SelectedRoleChips { get; set; }
+
+        public static UserDisplayModel Create(IdentityUser user, List<string> roles)
+        {
+            UserDisplayModel udm = new UserDisplayModel(); 
+            udm.Initialize(user, roles);
+            return udm;
+        }
+
+        private async Task InitializeAsync(UserManager<IdentityUser> userManager, string userName)
+        {
+            User = await userManager.FindByNameAsync(userName);
+            var roles = await userManager.GetRolesAsync(User);
+            SelectedRoles = roles.ToList();
+        }
+
+        private async Task InitializeAsync(UserManager<IdentityUser> userManager, IdentityUser user)
+        {
+            User = user;
+            var roles = await userManager.GetRolesAsync(User);
+            SelectedRoles = roles.ToList();
+        }
+
+        private void Initialize(IdentityUser user, List<string> roles)
+        {
+            User = user;
+            SelectedRoles = roles;
+        }
+
+        // Ensure only the Create function can call the constructor:
+        private UserDisplayModel()
+        { }
 
     }
 }
